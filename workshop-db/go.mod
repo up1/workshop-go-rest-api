799bdc6d589c0adb29b29ac8496bcebe5eb5076b
up1 <somkiat.p@gmail.com>
2021-08-07 09:36:21 +0700

    Add postgres driver

--- workshop-db/go.mod
+++ workshop-db/go.mod
@@ -1,3 +1,5 @@
 module demo
 
 go 1.16
+
+require github.com/lib/pq v1.10.2
--- workshop-db/main.go
+++ workshop-db/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"database/sql"
 	"log"
+
+	_ "github.com/lib/pq"
 )
 
 func main() {
@@ -10,11 +12,13 @@ func main() {
 }
 
 func createDatabaseConnection() *sql.DB {
-	db, err := sql.Open("postgres", "postgres://user:pass@localhost/demo?sslmode=disable")
+	var db *sql.DB
+	var err error
+
+	db, err = sql.Open("postgres", "postgres://user:pass@localhost/demo?sslmode=disable")
 	if err != nil {
 		log.Fatal(err)
 	}
-	// Ping to database
 	err = db.Ping()
 	if err != nil {
 		log.Fatal("Failed to ping DB: ", err)
